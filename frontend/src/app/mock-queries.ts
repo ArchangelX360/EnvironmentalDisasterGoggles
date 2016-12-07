4ef90aa99640a891d3204360d45cf10559b91831
Titouan BION <titouan.bion@gmail.com>
2016-12-07 17:23:10 +0100

    feat(frontend): monitoring UI with mocked queries

---
+++ frontend/src/app/mock-queries.ts
@@ -0,0 +1,21 @@
+import { Query } from './query';
+export let QUERIES: Query[] = [
+  {
+    "name": "Tous les incendies entre 2003 et 2009",
+    "token": "asedhafe654678ehh3526",
+    "status": "Superposition",
+    "progress": 15,
+  },
+  {
+    "name": "Déforestation en Amazonie en 2011",
+    "token": "wedsdfe654678ehhe3526",
+    "status": "Region partitionning",
+    "progress": 50,
+  },
+  {
+    "name": "Toutes les innnondations en France en 2010",
+    "token": "asedhafe654678ehh3526",
+    "status": "Image downloading",
+    "progress": 1,
+  }
+];
