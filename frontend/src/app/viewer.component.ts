acbadc848634ef9bc897ba62c4cd589f10bc77df
pierre-baptiste <bouillonpi@eisti.eu>
2016-12-08 13:40:56 +0100

    add: Response search-bar in viewer component

--- frontend/src/app/viewer.component.ts
+++ frontend/src/app/viewer.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchBarComponent } from './search-bar.component';
 
 @Component({
   selector: 'app-viewer',
-  templateUrl: './viewer.component.html'
+  templateUrl: './viewer.component.html',
 })
 export class ViewerComponent implements OnInit {
+
+  response: any;
+
   constructor() {
   }
 
   ngOnInit() {
   }
 
+  queryResponse(response) {
+    console.log("reponse dans le viewer", response);
+    this.response = response;
+  }
 }
