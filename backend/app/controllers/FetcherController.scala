603e39549cd0f1cf4ce0a8fa763bfcb22f46c90c
Damien Cassan <damien.cassan@eisti.eu>
2016-12-09 15:14:18 +0100

    feat(image-fetcher): Handle error from image-fetcher and reformat request arguments

--- backend/app/controllers/FetcherController.scala
+++ backend/app/controllers/FetcherController.scala
@@ -4,13 +4,15 @@ import javax.inject.Inject
 
 import akka.actor.ActorSystem
 import modules.imagefetcher.FetcherActor
-import play.api.mvc.{Action, Controller}
+import play.api.mvc.{Action, Controller, Result}
 import akka.pattern.ask
 import akka.util.Timeout
+import modules.imagefetcher.FetcherActor.{FetchRGB, FetchResponse}
 import play.api.libs.ws.WSClient
 
 import scala.concurrent.duration._
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
+
 
 /**
   * This controller exist for debug purposes, this give a direct access to the image fetcher service
@@ -22,21 +24,24 @@ class FetcherController @Inject() (system: ActorSystem, ws: WSClient, configurat
   /**
     * After this delay the server send a timeout, however the process is still running
     */
-  implicit val timeout: Timeout = 5.seconds
-
+  implicit val timeout: Timeout = configuration.underlying.getInt("pfs.servers.imageFetcherTimeout").seconds
 
   /**
-    * Ask the fetcher actor for an image an return an url to download that image
+    * Ask the fetcher actor for an image and return an url to download a zip of this image
     */
-  def fetch = Action.async { request =>
+  def fetchRGB = Action.async { request =>
     val body = request.body.asJson.get
-    (fetcherActor ? FetcherActor.FetchMessage(
-            start = (body \ "start").as[String],
-            end = (body \ "end").as[String],
-            position = (body \ "position").get
-      ))
-      .mapTo[FetcherActor.FetchResponse]
-      .map(response => Ok(response.url))
+    val response = fetcherActor ? FetchRGB (
+      start = (body \ "start").as[String],
+      delta = (body \ "delta").as[String],
+      scale = (body \ "scale").as[Double],
+      polygon = (body \ "position").get
+    )
+
+    response.map {
+      case FetchResponse(url) => Ok(url)
+      case error => InternalServerError(error.toString)
+    }
   }
 
 }
