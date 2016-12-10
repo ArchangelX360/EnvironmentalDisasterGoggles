5b2238c30994cc20101414592f46e5f9872b2ff6
Damien Cassan <damien.cassan@eisti.eu>
2016-12-10 18:05:39 +0100

    feat(monitoring): Add support for task creation and create a new task for the search module

--- backend/app/models/Query.scala
+++ backend/app/models/Query.scala
@@ -2,6 +2,7 @@ package models
 
 import play.api.libs.json._
 
+import scala.collection.mutable.ListBuffer
 
 /**
   * A tasks is a process realised by an actor toward the creation of a response to a query
@@ -11,9 +12,11 @@ case class Task(id: String, name: String, status: String, progress: Int)
 /**
   * Represent a query send by the user
   * @param name Content of the query (request)
+  * @param author Token for author authentication
+  * @param status Task progression
   * @param tasks List all tasks associated with this query
   */
-case class Query(id: String, author: String, name: String, status: String, tasks: List[Task])
+case class Query(id: String, author: String, name: String, status: String, tasks: ListBuffer[Task])
 
 /**
   * Companion object for Query class, contains implicit converters for tasks and query
