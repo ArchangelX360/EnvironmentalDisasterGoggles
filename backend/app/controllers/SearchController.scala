9a37594db41fb7cc7754d9c0444ab29ac7e6dab3
Damien Cassan <damien.cassan@eisti.eu>
2016-12-13 15:46:51 +0100

    feat(nlp): Extract places from the request using Stanford Parser

--- backend/app/controllers/SearchController.scala
+++ backend/app/controllers/SearchController.scala
@@ -28,7 +28,7 @@ class SearchController @Inject() (system: ActorSystem, schedulerService: Schedul
   /**
     * Do no send a response after this delay, the processing is not canceled anyway
     */
-  implicit val timeout: Timeout = 5.seconds
+  implicit val timeout: Timeout = 15.seconds
 
   /**
     * Forward the message to the search actor and notify the sender that the processing have been scheduled
