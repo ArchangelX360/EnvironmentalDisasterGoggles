975ab29dacdddd9d62e59d9c2559ebc30e6c0b3d
Damien Cassan <damien.cassan@eisti.eu>
2016-12-23 23:30:59 +0100

    refactor(imagefetcher): Improve file extraction and reformat imagefetcher module

--- backend/app/modules/imagefetcher/FetcherActor.scala
+++ backend/app/modules/imagefetcher/FetcherActor.scala
@@ -1,47 +1,47 @@
 package modules.imagefetcher
 
-
 import java.io.{File, FileOutputStream}
 
 import akka.actor.{Actor, ActorRef, Props}
+import akka.pattern.ask
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.Sink
 import akka.util.{ByteString, Timeout}
+import models.Task
 import modules.scheduler.MonitoringActor.{StartTask, UpdateTask}
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.libs.ws.WSClient
-import akka.pattern.ask
-import models.Task
 
 import scala.concurrent.duration._
 import scala.util.Random
-import scala.concurrent.duration._
 
 object FetcherActor {
 
   /**
-    * @param ws WebService to send http request to the flask server
-    * @param serverUrl Url of the flask server
+    * @param ws         WebService to send http request to the flask server
+    * @param serverUrl  Url of the flask server
+    * @param monitoring A reference to the monitoring actor
     */
-  def props(ws: WSClient, serverUrl: String, monitoring: ActorRef) = Props(new FetcherActor(ws, serverUrl, monitoring))
+  def props(ws: WSClient, serverUrl: String, monitoring: ActorRef): Props = Props(new FetcherActor(ws, serverUrl, monitoring))
 
-  /**
-    * Messages definitions
-    */
+  // Messages definitions
   case class FetchRGB(date: String, place: String, scale: Option[Double], queryId: String)
+
   case class FetchResponse(url: String)
-  case class DownloadFile(url: String, queryId: String)
+
+  case class DownloadFile(url: String, queryId: String, outputFolder: String)
+
   case class Downloaded(file: File)
+
 }
 
 class FetcherActor(ws: WSClient, serverUrl: String, monitoring: ActorRef) extends Actor {
 
+  // Import implicit definition
+  import FetcherActor._
+
   implicit val timeout: Timeout = 5.seconds
 
-  /**
-    * Import implicit definition
-    */
-  import FetcherActor._
   implicit val materializer = ActorMaterializer()
 
   /**
@@ -49,14 +49,16 @@ class FetcherActor(ws: WSClient, serverUrl: String, monitoring: ActorRef) extend
     */
   override def receive: Receive = {
     case message: FetchRGB => fetchImage(message)
-    case DownloadFile(url, queryId) => downloadFile(url, queryId)
+    case DownloadFile(url, queryId, outputFolder) => downloadFile(url, queryId, outputFolder)
     case _ => sender() ! "Image Fetcher not yet implemented"
   }
 
   /**
     * Request the flask server to process image from Earth Engine and return the url to download the image
+    *
+    * @param message Details of the request sent to the python server
     */
-  def fetchImage(message: FetchRGB) = {
+  def fetchImage(message: FetchRGB): Unit = {
 
     val task = monitoring.ask(StartTask(message.queryId, "Fetching image"))(50.seconds).mapTo[Task]
 
@@ -69,48 +71,51 @@ class FetcherActor(ws: WSClient, serverUrl: String, monitoring: ActorRef) extend
       else initialParams
 
     val request = ws.url(serverUrl + "/rgb")
-        .withQueryString(params:_*)
-        .get()
+      .withQueryString(params: _*)
+      .get()
 
     val currentSender = sender
 
-    request.map ( response =>
-        if (response.status == 200) {
-          task map (t => monitoring ! UpdateTask(message.queryId, t.id, Some("Image generated on Earth Engine"), Some(100)))
-          val url = (response.json \ "href").as[String]
-          currentSender ! FetchResponse(url)
-        } else {
-          task map (t => monitoring ! UpdateTask(message.queryId, t.id, Some("Link generatation failed"), Some(0)))
-          val error = (response.json \ "error").asOpt[String]
-          currentSender ! "An error occurred during image fetching: " + error.getOrElse("no details")
-        }
+    request.map(response =>
+      if (response.status == 200) {
+        task map (t => monitoring ! UpdateTask(message.queryId, t.id, Some("Image generated on Earth Engine"), Some(100)))
+        val url = (response.json \ "href").as[String]
+        currentSender ! FetchResponse(url)
+      } else {
+        task map (t => monitoring ! UpdateTask(message.queryId, t.id, Some("Link generation failed"), Some(0)))
+        val error = (response.json \ "error").asOpt[String]
+        currentSender ! "An error occurred during image fetching: " + error.getOrElse("no details")
+      }
     )
   }
 
   /**
-    * Download a file from an url through an akka stream an place it in the downloaded folder
-    * (Downloaded file is not labeled yet e.g random name)
+    * Download a file from an url through an akka stream and place it in the downloaded folder
+    * (Downloaded file is not labeled yet, we use a random id)
+    *
+    * @param url          Url from which to download the file
+    * @param processId    Identifier of the process to which this download is related (for monitoring purpose)
+    * @param outputFolder Folder in which the file will be downloaded
     */
-  def downloadFile(url: String, processId: String) = {
+  def downloadFile(url: String, processId: String, outputFolder: String): Unit = {
 
     val task = monitoring.ask(StartTask(processId, "Downloading image")).mapTo[Task]
-
     val currentSender = sender
 
-    val downloaded = new File("Downloaded").mkdir()
+    // Create the directory to store downloaded files
+    new File(outputFolder).mkdir()
     val file = new File("Downloaded/" + Random.nextInt() + ".zip")
     val response = ws.url(url).withMethod("GET").stream()
 
-    val downloadedFile = response.flatMap(res => {
+    response.flatMap(res => {
 
       task.map(t => monitoring ! UpdateTask(processId, t.id, Some("Download started"), Some(20)))
 
       val outputStream = new FileOutputStream(file)
-
       val sink = Sink.foreach[ByteString](bytes => outputStream.write(bytes.toArray))
 
       res.body.runWith(sink).andThen {
-        case result =>
+        case _ =>
           outputStream.close()
           task.map(t => monitoring ! UpdateTask(processId, t.id, Some("Download finished"), Some(100)))
           currentSender ! Downloaded(file)
--- backend/app/modules/scheduler/SchedulerActor.scala
+++ backend/app/modules/scheduler/SchedulerActor.scala
@@ -1,14 +1,12 @@
 package modules.scheduler
 
-import java.io.File
-
 import akka.actor.{Actor, ActorRef, Props}
 import akka.pattern.ask
 import akka.util.Timeout
 import models.Query
 import models.Query.Queries
-import modules.imagefetcher.{FetcherActor, ZipUtil}
 import modules.imagefetcher.FetcherActor.{DownloadFile, Downloaded, FetchRGB, FetchResponse}
+import modules.imagefetcher.{FetcherActor, ZipUtil}
 import modules.scheduler.MonitoringActor.{GetProcess, UpdateProcess}
 import modules.scheduler.SchedulerActor.{CancelProcessing, StartProcessing}
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
@@ -23,11 +21,11 @@ object SchedulerActor {
             monitoring: ActorRef): Props =
     Props(new SchedulerActor(processes, configuration, ws, monitoring))
 
-  /**
-    * Message definitions
-    */
+  //Message definitions
   case class StartProcessing(id: String)
+
   case class CancelProcessing(id: String)
+
 }
 
 /**
@@ -39,6 +37,9 @@ class SchedulerActor(processes: Queries, configuration: play.api.Configuration, 
 
   implicit val timeout: Timeout = 50.seconds
 
+  // TODO: Move this property to a config file
+  val outputFolder = "Downloaded"
+
   override def receive: Receive = {
     case StartProcessing(id) => startProcessing(id)
     case CancelProcessing(id) => cancelProcessing(id)
@@ -46,35 +47,38 @@ class SchedulerActor(processes: Queries, configuration: play.api.Configuration, 
 
   /**
     * Entry point of the pipeline
-    * * Send query to the search engine
-    * * Download image
+    * First it send query to the search engine which gather parameters such as places and dates.
+    * Then the the request is send to the python server which generate an url which is used to download images
+    * Finally, the image is extracted from the downloaded zip
+    *
+    * @param id Identifier of the process to start (The process must already have been initialized by the search engine)
     */
   def startProcessing(id: String): Unit = {
 
     // Gather details about the query and register tasks with the monitoring actor
     val details = monitoring
-        .ask(GetProcess(id))
-        .mapTo[Query]
-        .map(query => query.details)
-        .map {
-          case Some(detail) => detail
-          case _ =>
-            monitoring ! UpdateProcess(id, "failed: no details")
-            throw new Exception("no details")
-        }
+      .ask(GetProcess(id))
+      .mapTo[Query]
+      .map(query => query.details)
+      .map {
+        case Some(detail) => detail
+        case _ =>
+          monitoring ! UpdateProcess(id, "failed: no details")
+          throw new Exception("no details")
+      }
 
     // Send processing request to Google Earth Engine using the python server
     val url = details
-      .flatMap(details => fetcherActor.ask(FetchRGB("2015-01-01", details.place, Some(100), id)))
+      .flatMap(details => fetcherActor.ask(FetchRGB("2015-01-01", details.place, None, id)))
       .mapTo[FetchResponse]
 
     // Download result from Earth Engine
     val zip = url
-      .flatMap(response => fetcherActor.ask(DownloadFile(response.url, id)))
+      .flatMap(response => fetcherActor.ask(DownloadFile(response.url, id, outputFolder)))
       .mapTo[Downloaded]
 
     // Extract image from the zip file
-    val images = zip map (downloaded => ZipUtil.extractZip(downloaded.file))
+    val images = zip map (downloaded => ZipUtil.extractZip(downloaded.file, outputFolder = outputFolder))
 
     // Add further processing here
     images foreach (file => println(file.map(f => f.getAbsolutePath).getOrElse("file not found in zip")))
