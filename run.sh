95440b19bc71f623c1cb334ee40d532d87c1eb06
Titouan BION <titouan.bion@gmail.com>
2017-01-08 23:49:10 +0100

    fix(install-run-scripts): secret conf token + background dockers

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker run -p 5000:5000 imagefetcher
-docker run -p 3030:3030 fuseki-server
+docker run -p 5000:5000 imagefetcher &
+docker run -p 3030:3030 fuseki-server &
 
-backend/target/universal/environmentaldisastergoggles-1.0/bin/environmentaldisastergoggles
+backend/target/universal/environmentaldisastergoggles-1.0/bin/environmentaldisastergoggles -Dplay.crypto.secret=eisti
 
