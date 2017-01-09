afe252ca57377ffd6a5ca1aba51f5a1ff81c9d53
Damien Cassan <damien.cassan@eisti.eu>
2017-01-09 01:08:16 +0100

    fix(frontend): Use hash url

--- frontend/src/app/app.routing.ts
+++ frontend/src/app/app.routing.ts
@@ -15,4 +15,6 @@ const appRoutes: Routes = [
 
 export const appRoutingProviders: any[] = [];
 
-export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes, {
+  useHash: true
+});
